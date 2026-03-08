27feffc3dde354717b5fce82278c196d25edfa48
DWTY02GROUP3-IWP <24FTT1128@student.pb.edu.bn>
2026-03-09 02:35:57 +0800

    Made the name into Hydrapulse.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Hydrapulse')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('head')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
