c306fdd3de50677df8103451bdb90d05514570be
Nani Aliasin <24FTT1096@student.pb.edu.bn>
2026-04-23 04:25:12 +0800

    app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Add this line at the top
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Add these 3 lines here
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
